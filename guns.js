5520f5e2a24e6c6b57fab1ce034986ad42082c2f
YT-QUICKGAM3R <66910237+YT-QUICKGAM3R@users.noreply.github.com>
2022-06-29 17:36:11 +0100

    Create guns.js

---
+++ guns.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const FARA = new Discord.MessageEmbed()
+  .setTitle('FARA 83')
+  .setDescription('Here are the FARA 83 wepon stats')
+  .addField('Headshot damage', '40')
+  .addField('Chest damage', '31',
+  )
+  .addField('Shoulder damage', '31')
+  .addField('Extremities damage', '31')
+  .setImage(
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.invenglobal.com%2Farticles%2F13381%2Fguide-best-loadout-for-the-fara-83-assault-rifle-in-cod-warzone&psig=AOvVaw3aoqoTrHdCB2HGJQyph6VU&ust=1623946094442000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKio0ePEnPECFQAAAAAdAAAAABAD',
+  );
